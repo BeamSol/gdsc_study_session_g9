9aa59f78587f3914466e24882ad50fe0f2950675
BeamSol <141314118+BeamSol@users.noreply.github.com>
2023-12-31 16:00:58 +0300

    Add files via upload

---
+++ Group Members/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import GroupMembers from './GroupMembers'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <GroupMembers/>
+      </div>
+    </>
+  )
+}
+
+export default App
